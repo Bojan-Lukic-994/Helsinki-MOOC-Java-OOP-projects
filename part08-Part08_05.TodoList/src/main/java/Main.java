d285fa757bb3e7223228dfd7d902fea956bbd566
Bojan-Lukic-994 <40005983+Bojan-Lukic-994@users.noreply.github.com>
2021-01-31 16:32:51 +0100

    Update Main.java

--- part08-Part08_05.TodoList/src/main/java/Main.java
+++ part08-Part08_05.TodoList/src/main/java/Main.java
@@ -1,12 +1,21 @@
 /*
 In this exercise we are going to create a program that can be used to create and modify a to-do list.
 
-PART1: Create a class called TodoList. It should have a constructor without parameters and the following methods:
+PART1: 
+Create a class called TodoList. It should have a constructor without parameters and the following methods:
 
 public void add(String task) - add the task passed as a parameter to the todo list.
 public void print() - prints the exercises. Each task has a number associated with it on the print statement — use the task's index here (+1).
 public void remove(int number) - removes the task associated with the given number; the number is the one seen associated with the task in the print.
- */
+ 
+PART2:
+Create a class called TodoList. It should have a constructor without parameters and the following methods:
+
+public void add(String task) - add the task passed as a parameter to the todo list.
+public void print() - prints the exercises. Each task has a number associated with it on the print statement — use the task's index here (+1).
+public void remove(int number) - removes the task associated with the given number; the number is the one seen associated with the task in the print.
+*/
+
 import java.util.Scanner;
 
 public class Main {
